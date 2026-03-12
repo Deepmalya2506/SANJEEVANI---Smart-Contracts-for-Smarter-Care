cc33db25d2c4e1389e8ac6f7fecbfafb36a82c15
Deepmalya2506 <iamdeepmalya@gmail.com>
2026-03-12 23:54:58 +0530

    Created eqp.py, db.py,

---
+++ mcp/models/equipment.py
@@ -0,0 +1,16 @@
+equipment_schema = {
+    "hospital_id": "HOSP_A",
+
+    "equipment_type": "OXYGEN_CYLINDER",
+
+    "assets": [
+        {
+            "asset_uid": "OXY001",
+            "status": "AVAILABLE"
+        },
+        {
+            "asset_uid": "OXY002",
+            "status": "IN_USE"
+        }
+    ]
+}
